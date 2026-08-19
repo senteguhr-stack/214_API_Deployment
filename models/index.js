1b10f42be8802647d65c177d47a3c72b2550863a
senteguhr <senteguhr@gmail.com>
2026-08-19 13:58:09 +0700

    fix

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+const pg = require('pg');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
